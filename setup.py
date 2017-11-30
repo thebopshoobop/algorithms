c276e9a02b4d828bbb0c4d29e99271a0ee90b9d0
Will Timpson <will.timpson@gmail.com>
2017-11-29 23:35:21 -0500

    outline

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="",
+    version="0.0.1",
+    author="Will Timpson",
+    description=(""),
+    license="ISC",
+    packages=[],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest']
+)
