ed2380acc7c978b08a16ec90b05e2465fe7cee65
Will Timpson <will.timpson@gmail.com>
2017-11-30 00:34:10 -0500

    javascript merge sort

---
+++ spec/sortSpec.js
@@ -0,0 +1,49 @@
+const mergeSort = require("../src/mergeSort");
+
+const sorts = [mergeSort];
+
+const verify = (output, result) => {
+  if (result.length !== output.length) {
+    return false;
+  }
+  for (let i = 0; i < result.length; i++) {
+    if (result[i] !== output[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const checkSort = input => sort_func => {
+  const output = input.slice().sort((a, b) => a - b);
+  const result = sort_func(input.slice());
+
+  expect(verify(output, result)).toBe(true);
+};
+
+const randomSample = () => {
+  return [...Array(100)].map(() => Math.floor(Math.random() * 200) - 100);
+};
+
+describe("Sorts", () => {
+  it("handles a small test case", () => {
+    sorts.forEach(checkSort([1, 5, 2, 5, 7, 3]));
+  });
+
+  it("handles a large random sample", () => {
+    sorts.forEach(checkSort(randomSample()));
+  });
+
+  it("handles a large sorted sample", () => {
+    sorts.forEach(checkSort(randomSample().sort()));
+  });
+
+  it("handles a large reversed sample", () => {
+    sorts.forEach(checkSort(randomSample().reverse()));
+  });
+
+  it("handles a large identical sample", () => {
+    const sample = Math.floor(Math.random() * 200) - 100;
+    sorts.forEach(checkSort(new Array(100).fill(sample)));
+  });
+});
---
+++ src/mergeSort.js
@@ -0,0 +1,32 @@
+const merge_sort = sample => {
+  if (sample.length < 2) {
+    return sample;
+  }
+
+  const pivot = Math.floor(Math.random() * sample.length);
+  const left = merge_sort(sample.slice(0, pivot));
+  const right = merge_sort(sample.slice(pivot));
+  let [lCurrent, rCurrent] = [0, 0];
+  const [lMax, rMax] = [left.length, right.length];
+
+  let merged = [];
+  while (lCurrent < lMax && rCurrent < rMax) {
+    if (left[lCurrent] < right[rCurrent]) {
+      merged.push(left[lCurrent]);
+      lCurrent++;
+    } else {
+      merged.push(right[rCurrent]);
+      rCurrent++;
+    }
+  }
+
+  if (lCurrent - lMax) {
+    merged = merged.concat(left.slice(lCurrent - lMax));
+  } else if (rCurrent - rMax) {
+    merged = merged.concat(right.slice(rCurrent - rMax));
+  }
+
+  return merged;
+};
+
+module.exports = merge_sort;
